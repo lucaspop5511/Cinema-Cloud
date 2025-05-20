b7455d71455fe6663d27eabf83d9d9603490438a
Lucas <lucaspop5511@gmail.com>
2025-05-20 18:28:41 +0300

    added fonts

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-// Import only the CSS files we're keeping
+import './styles/fonts.css';
 import './styles/index.css'
 import './styles/App.css'
 import './styles/Header.css'
@@ -18,14 +18,10 @@ import './styles/Cinema.css'
 import './styles/SearchResults.css'
 import './styles/Detail.css'
 import './styles/Watchlist.css'
-// Import auth styles
 import './styles/auth/LoginModal.css'
 import './styles/auth/AuthButton.css'
-// Import watchlist styles
 import './styles/WatchlistButton.css'
-// Import button styles
 import './styles/NowPlayingButton.css'
-// Import streaming providers styles
 import './styles/StreamingProviders.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
